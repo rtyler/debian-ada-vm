f2ab72ef50714cf918987b465a2afaed12a257de
R. Tyler Croy <tyler@monkeypox.org>
2011-08-01 15:54:54 -0700

    Add a simple manifest for now to enumerate all the Ada packages I want

---
+++ manifests/base.pp
@@ -0,0 +1,67 @@
+#
+#   Base manifest for the Debian Ada Virtual Machine
+#
+
+
+node default {
+    package {
+        # Compiler
+        "ada-compiler"      :
+            ensure  => installed;
+
+
+        # Helpful development utilities
+        "gprbuild"          :
+            ensure  => installed;
+        "libaunit3"         :
+            ensure  => installed;
+        "libaunit1-dev"     :
+            ensure  => installed;
+        "adabrowse"         :
+            ensure  => installed;
+
+
+        # Documentation
+        "ada-reference-manual-text" :
+            ensure  => installed;
+        "ada-reference-manual-info" :
+            ensure  => installed;
+
+
+        # Additional libraries
+        "libxmlada3.2"      :
+            ensure  => installed;
+        "libxmlada3.2-dev"  :
+            ensure  => installed;
+        "libxmlada-doc"     :
+            ensure  => installed;
+
+        "libadasockets3"    :
+            ensure  => installed;
+        "libadasockets3-dev":
+            ensure  => installed;
+
+        "liblog4ada0"       :
+            ensure  => installed;
+        "liblog4ada1-dev"   :
+            ensure  => installed;
+
+
+        # Web Development
+        "libaws-bin"        :
+            ensure  => installed;
+        "libaws-doc"        :
+            ensure  => installed;
+        "libaws2.7"         :
+            ensure  => installed;
+        "libaws2.7-dev"     :
+            ensure  => installed;
+
+
+        # Misc helpful utilities
+        "gdb"               :
+            ensure  => installed;
+        "cgdb"              :
+            ensure  => installed;
+    }
+}
